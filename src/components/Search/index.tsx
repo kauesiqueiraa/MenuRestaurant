c36e7833c9423a4b61c1f7f11df4b8ec94154bac
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-10-02 22:07:00 -0300

    feat: create page Search and scss Styles

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from './Search.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  search: string,
+  setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Search({ search, setSearch }: Props) {
+  return (
+    <div className={styles.search}>
+      <input
+        type="text"
+        value={search}
+        onChange={event => setSearch(event.target.value)}
+      />
+      <CgSearch
+        size={20}
+        color="#4C4D5E"
+      />
+    </div>
+  )
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,11 +1,30 @@
-import style from './Menu.module.scss';
+import Search from 'components/Search';
+import styles from './Menu.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import { useState } from 'react';
 
 export default function Menu() {
+  const [search, setSearch] = useState("");
+
+
   return (
     <main>
-      <nav className={style.menu}>
-
+      <nav className={styles.menu}>
+        <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do Código e da Massa
+        </div>
+      </header>
+
+      <section className={styles.section_menu}>
+        <h3 className={styles.section_menu__title}>Cardápio</h3>
+        <Search
+          search={search}
+          setSearch={setSearch}
+        />
+      </section>
     </main>
   )
 }
