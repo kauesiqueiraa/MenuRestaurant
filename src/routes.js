5c2c5717e8ae418a1fa988a0bb9856e590c492e4
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-10-09 20:59:59 -0300

    feat: page NotFound and routes configuration

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from "components/Navbar";
-
 import Home from "pages/Home";
 import Menu from "pages/Menu";
 import About from "pages/About";
+import NotFound from "pages/NotFound";
+
 import DefaultPage from "components/DefaultPage";
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className="container">
       <Router>
         <Navbar />
         
@@ -19,8 +21,10 @@ export default function AppRouter() {
             <Route path="menu" element={<Menu />} />
             <Route path="about" element={<About />} />
           </Route>
-          
+          <Route path="*" element={<NotFound />} />
         </Routes>
+
+        <Footer />
       </Router>
     </main>
   );
